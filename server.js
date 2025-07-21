8bdd7907701f9325a776c9d5fa4ead6e89bad8f6
Lyan151 <lyanangell028@gmail.com>
2025-07-21 15:13:14 -0300

    UPDATE

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const sequelize = require('./config/db');
 require('dotenv').config();
 const express = require('express')
  
-const userRoutes = require('./routes/userRoutes')
- 
+const cors = require('cors');
+
+const userRoutes = require('./routes/userRoutes');
+const contactRoutes = require('./routes/contactRoutes');
+const authRoutes = require('./routes/authRoutes');
 const app = express();
- 
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
  
 app.get('/', (req, res)=> res.send('api funcionando'))
  
 app.use('/api/users', userRoutes)
- 
+ app.use('/api/contact' , contactRoutes)
 const PORT = process.env.PORT;
  
 sequelize.authenticate()
