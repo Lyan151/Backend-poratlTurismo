da7656adf7f268019d37ef5d890554148c2db865
Lyan151 <lyanangell028@gmail.com>
2025-07-29 16:23:28 -0300

    UPDATE

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const login = require('../controllers/authController')
+const auth = require('../controllers/authController');
 
 router.post('/login', auth.login )
 
